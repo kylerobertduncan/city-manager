b08078efc79e890979152b3e2c6641fa8ca7167f
Kyle <kylerobertduncan@gmail.com>
2024-02-01 09:42:53 -0500

    Adds fonts, css baseline and theme. Adds header to sidebar.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,94 @@
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+// import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+// import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
 
 export default function Sidebar() {
   return(
     <Grid item component="aside" xs={12} lg={3}>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography
+            variant="h5"
+            noWrap
+            component="h1"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            City Manager
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
       <Container>
-        <Typography component="h1" variant="h3">
-          City Manager
-        </Typography>
       </Container>
     </Grid>
   )
---
+++ src/theme.tsx
@@ -0,0 +1,52 @@
+import { createTheme } from "@mui/material/styles";
+// import { responsiveFontSizes } from "@mui/material/styles";
+
+// https://mui.com/material-ui/customization/theming/#typescript
+// declare module '@mui/material/styles' {
+//   interface Theme {
+//     status: {
+//       danger: string;
+//     };
+//   }
+//   // allow configuration using `createTheme`
+//   interface ThemeOptions {
+//     status?: {
+//       danger?: string;
+//     };
+//   }
+// }
+
+// A custom theme for this app
+let theme = createTheme({
+  palette: { mode: 'dark' },
+  typography: {
+    /*
+    The main font, used for the section titles is called BondiSvtyITCTT-Bold
+    "Name", "Player", etc is a version of Cormorant as is most of the other fonts on the official sheet
+    Health, Willpower, etc is Gill Sans.  
+    font-family: 'Bodoni Moda', serif;
+    font-family: 'Cormorant', serif;
+    font-family: 'Cormorant Garamond', serif;
+    font-family: 'EB Garamond', serif;
+    font-family: 'Lato', sans-serif; (Gill Sans Nova substitute)
+    */
+    fontFamily: "Cormorant, serif",
+    h1: {
+      fontFamily: "Bodoni Moda, serif",
+    },
+    h2: {
+      fontFamily: "Bodoni Moda, serif",
+    },
+    body2: {
+      fontFamily: "Lato, sans-serif",
+    }
+  },
+});
+
+// adds responsive font sizes for mobile
+// theme = responsiveFontSizes(theme, {factor: 3});
+
+// log theme details for customization
+// console.debug(theme);
+
+export default theme;
