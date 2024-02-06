3cfa189486e629094eaf97de57bbd1a0182241f2
Kyle <kylerobertduncan@gmail.com>
2024-02-06 12:01:07 -0500

    Installs Prettier and updates core components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import SidebarHeader from './SidebarHeader';
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import SidebarHeader from "./SidebarHeader";
 // import Typography from '@mui/material/Typography';
 
 /*
@@ -9,12 +9,10 @@ If present, add them to the sidebar
 */
 
 export default function Sidebar() {
-  return(
-    <Grid item component="aside" xs={12} lg={3}>
-      <SidebarHeader />
-      <Container>
-        
-      </Container>
-    </Grid>
-  )
+	return (
+		<Grid item component="aside" xs={12} lg={3}>
+			<SidebarHeader />
+			<Container></Container>
+		</Grid>
+	);
 }
